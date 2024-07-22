695e1701a5bc29fac6c574069ec99e827f6f82b6
ali-alaalem <Meemo2002a@gmail.com>
2024-07-22 15:12:24 +0300

    adding insurance

--- models/cars.js
+++ models/cars.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const carSchema = new mongoose.Schema({
   name: String,
   price: Number,
+  insurance: String,
 });
 
 const Car = mongoose.model("Car", carSchema);
 module.exports = Car;
+  
